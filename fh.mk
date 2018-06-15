c8774e1593e93f093f965fc466051539f6b9059b
dylanneve1 <dylanneve1@gmail.com>
2018-06-15 21:40:07 +0100

    FIRE-UP

--- fh.mk
+++ fh.mk
@@ -23,13 +23,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common FireHound stuff.
+$(call inherit-product, vendor/fh/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_herolte
+PRODUCT_NAME := fh_herolte
 PRODUCT_DEVICE := herolte
 PRODUCT_MODEL := SM-G930F
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Dylan Neve (dylanneve1)"
